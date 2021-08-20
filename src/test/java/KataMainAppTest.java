4b88cd0edb480baf516332ae63cbabaf30afe1b5
Kousik <kousikthuli143@gmail.com>
2021-08-20 11:23:50 +0530

    Junit Test Case

---
+++ src/test/java/KataMainAppTest.java
@@ -0,0 +1,52 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class KataMainAppTest{
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void testCase1() {
+        String simulatedUserInput = "Player1\nPlayer2\n1\n1\n1\n";
+        provideInput(simulatedUserInput);
+        KataMainApp.main(new String[0]);
+    }
+
+}
